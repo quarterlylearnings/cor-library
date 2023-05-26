454e232baf3174f62ec9065439bb9a867311fa06
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2023-05-26 13:09:15 -0400

    Modifies initial books for Library state

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,53 +1,35 @@
-import { useState } from "react"
-import Shelf from "./Shelf"
+import { useState } from "react";
+import Shelf from "./Shelf";
 
 const Library = () => {
-    const [shelves, setShelves] = useState([
+  const [shelves, setShelves] = useState([
     {
-      genre: 'Fiction',
+      genre: "Fiction",
       books: [
-        { title: 'To Kill a Mockingbird', author: 'Harper Lee' },
-        { title: '1984', author: 'George Orwell' },
-        { title: 'Beloved', author: 'Toni Morrison' },
-        { title: 'The Color Purple', author: 'Alice Walker' },
+        { title: "To Kill a Mockingbird", author: "Harper Lee" },
+        { title: "1984", author: "George Orwell" },
+        { title: "Beloved", author: "Toni Morrison" },
+        { title: "The Color Purple", author: "Alice Walker" },
       ],
     },
     {
-      genre: 'Mystery',
+      genre: "Mystery",
       books: [
-        { title: 'Gone Girl', author: 'Gillian Flynn' },
-        { title: 'The Underground Railroad', author: 'Colson Whitehead' },
-        { title: 'IQ', author: 'Joe Ide' },
-        { title: 'Blacktop Wasteland', author: 'S. A. Cosby' },
+        { title: "Gone Girl", author: "Gillian Flynn" },
+        { title: "The Underground Railroad", author: "Colson Whitehead" },
+        { title: "IQ", author: "Joe Ide" },
+        { title: "Blacktop Wasteland", author: "S. A. Cosby" },
       ],
     },
-    {
-      genre: 'Science Fiction',
-      books: [
-        { title: 'Kindred', author: 'Octavia Butler' },
-        { title: 'Dawn', author: 'Octavia Butler' },
-        { title: 'Parable of the Sower', author: 'Octavia Butler' },
-        { title: 'Binti', author: 'Nnedi Okorafor' },
-      ],
-    },
-    {
-      genre: 'Fantasy',
-      books: [
-        { title: 'Children of Blood and Bone', author: 'Tomi Adeyemi' },
-        { title: 'Akata Witch', author: 'Nnedi Okorafor' },
-        { title: 'Black Leopard, Red Wolf', author: 'Marlon James' },
-        { title: 'The Fifth Season', author: 'N. K. Jemisin' },
-      ],
-    },
-  ])
+  ]);
 
-    return (
-        <section>
-            {
-              shelves.map((shelf, index) => <Shelf key={index} genre={shelf.genre} books={shelf.books} />)  
-            }
-        </section>
-    )
-}
+  return (
+    <section>
+      {shelves.map((shelf, index) => (
+        <Shelf key={index} genre={shelf.genre} books={shelf.books} />
+      ))}
+    </section>
+  );
+};
 
-export default Library
+export default Library;
